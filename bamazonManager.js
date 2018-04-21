433748c260a64d8421288ae285f6b785de0f56e9
olivas1406 <olivas1406@msn.com>
2018-04-20 21:32:36 -0700

    app is working, low inventory returns Undefined for price, needs comments

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,10 +1,4 @@
 
-/*
-
-
-If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
-
-*/
 
 var what = 0;
 
@@ -20,7 +14,7 @@ var connection = mysql.createConnection({
     user: "root",       
     password: "tiMh9AwUOkKzsfX1wVBs",  
     database: "bamazon" 
-})
+});
 
 inquirer.prompt([{
     type: 'rawlist',
@@ -44,9 +38,8 @@ inquirer.prompt([{
 
         case "Add New Product": 
         addProd();
-
     }   
-})
+});
 
 function viewDB() {
     connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, res)  {  
@@ -102,21 +95,16 @@ function addInv() {
             var query = "SELECT stock_quantity FROM products WHERE item_id = " + what;
 
             connection.query(query, function(err, res) {   
-                if (err) throw err
-                
-            howMany = +howMany + +(JSON.stringify(res[0].stock_quantity));
-
-            updateInv();
-
-            return what, howMany;
-    
-        })
+                if (err) throw err                
+                howMany = +howMany + +(JSON.stringify(res[0].stock_quantity));
+                updateInv();
+                return what, howMany;
+            })
         }
     });
 }
 
 function updateInv() {
-
     var query2 = "UPDATE products SET stock_quantity = " + howMany + " WHERE item_id = " + what;  
         connection.query(query2, function(err,) {   
             if (err) throw err
@@ -125,9 +113,7 @@ function updateInv() {
             connection.end();
 };
 
-
 function addProd() {
-
     inquirer.prompt([{
         message: "What product would you like to add?",
         type: "input",
@@ -146,21 +132,19 @@ function addProd() {
         type: "input",
         name: "stock"
     
-    
-
-/////////////// UPDATE THIS SECTION TO DEAL WITH EMPTY FIELDS WHEN ADDING A PRODUCT ////////////////////////////
-
     }]).then(function(answer){       
         if (answer.prodName.length === 0) {
-            console.log("Please enter a valid quantity")
-
-
-            //  addProd();                                      // PUT THIS BACK IN
-
-/////////////// UPDATE THIS SECTION TO DEAL WITH EMPTY FIELDS WHEN ADDING A PRODUCT ////////////////////////////
-
-
-
+            console.log("Please enter a Product to add");
+            addProd();
+        } else if (answer.deptName.length === 0) {
+            console.log("Please enter a Department");
+            addProd();
+        } else if (answer.price.length === 0) {
+            console.log("Please enter a Price"); 
+            addProd();
+        } else if (answer.stock.length === 0) {
+            console.log("Please enter an Inventory amount");
+            addProd();       
         } else {
             connection.query('INSERT INTO products SET ?',{
                 product_name: answer.prodName,
